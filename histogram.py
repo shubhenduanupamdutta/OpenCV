876ef376f6a0cf2ae840e65107e6a055040001c5
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-10-26 21:12:03 +0530

    Masking and calculating intensity histogram
    Masking an image and then using OpenCV to calculate intensity
    distribution for a gray scale image. histogram plot shown using
    matplotlib pyplot.

--- histogram.py
+++ histogram.py
@@ -1,19 +1,32 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
+import numpy as np
 
 # NOTE: Histograms allow us to visualize intensity distribution in an image.
 
 image = cv.imread("./Resources/Photos/cats 2.jpg")
 cv.imshow("Original Image", image)
 
+# Create a blank image to create a mask
+blank = np.zeros(image.shape[:2], dtype=np.uint8)
+
+
 # Histogram of grayscale image
 # First covert the image to grayscale
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 cv.imshow("Grayscale Image", gray)
 
+# Create a mask
+mask = cv.circle(blank.copy(), (image.shape[1] // 2, image.shape[0] // 2),
+                 100, (255, 255, 255), -1)
+# cv.imshow("Mask", mask)
+
+masked_image = cv.bitwise_and(gray, gray, mask=mask)
+# cv.imshow("Masked Grayscale image", masked_image)
+
 # Grayscale histogram
 gray_hist = cv.calcHist(images=[gray], channels=[
-                        0], mask=None, histSize=[256], ranges=[0, 255])
+                        0], mask=mask, histSize=[256], ranges=[0, 255])
 
 plt.figure()
 plt.title("Grayscale Histogram")
